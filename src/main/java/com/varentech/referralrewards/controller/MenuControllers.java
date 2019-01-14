2123a3f40f927d7339aec8ec16936ecdcc59941a
parksWill <williamparks94@gmail.com>
2019-01-14 16:35:45 -0500

    [feature] History Page - reformatted code in MenuControllers - added controller for history page

--- src/main/java/com/varentech/referralrewards/controller/MenuControllers.java
+++ src/main/java/com/varentech/referralrewards/controller/MenuControllers.java
@@ -13,43 +13,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MenuControllers {
 
-    @Autowired
-    private PointsService pointService;
+  @Autowired
+  private PointsService pointService;
 
-    @GetMapping("/home")
-    public String processHome(Model model){
-        System.out.println("reached");
-        return "index";
+  @GetMapping("/home")
+  public String processHome(Model model) {
+    System.out.println("reached");
+    return "index";
+  }
+
+  @Autowired
+  DefaultUserDetailsService defaultUserDetailsService;
+
+  @GetMapping("/info")
+  public String processInfo(Model model, @AuthenticationPrincipal DefaultUser user) {
+    if (user.getAuthorities().contains(new SimpleGrantedAuthority("recruiter")) || user
+        .getAuthorities().contains(new SimpleGrantedAuthority("admin")) || user.getAuthorities()
+        .contains(new SimpleGrantedAuthority("superadmin"))) {
+      return "infoForRecruiters";
+    } else {
+      return "infoForUsers";
     }
+  }
 
-    @Autowired
-    DefaultUserDetailsService defaultUserDetailsService;
+  @GetMapping("/leaderboard")
+  public String processLeaderboardInfo(Model model) {
+    return "leaderboard";
+  }
 
-    @GetMapping("/info")
-    public String processInfo(Model model, @AuthenticationPrincipal DefaultUser user){
-        if(user.getAuthorities().contains( new SimpleGrantedAuthority("recruiter")) || user.getAuthorities().contains( new SimpleGrantedAuthority("admin")) || user.getAuthorities().contains( new SimpleGrantedAuthority("superadmin"))){
-            return "infoForRecruiters";
-        }
-        else{
-            return "infoForUsers";
-        }
+  @GetMapping("/history")
+  public String processHistory() {
+    return "history";
+  }
 
+  @GetMapping("/prizes")
+  public String processPrizes(Model model, @AuthenticationPrincipal DefaultUser user) {
+    Long pointsThisYear = pointService.getPointsLastYearByEmployeeId(user.getId());
+    model.addAttribute("pointsThisYear", pointsThisYear);
 
-
-    }
-
-    @GetMapping("/leaderboard")
-    public String processLeaderboardInfo(Model model){
-        System.out.println("reached");
-        return "leaderboard";
-    }
-
-    @GetMapping("/prizes")
-    public String processPrizes(Model model, @AuthenticationPrincipal DefaultUser user){
-        Long pointsThisYear = pointService.getPointsLastYearByEmployeeId(user.getId());
-        model.addAttribute("pointsThisYear", pointsThisYear);
-
-        return "prizes";
-    }
+    return "prizes";
+  }
 
 }
